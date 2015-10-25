72e7e17346169c9d8cbf22fcbd700e7f8557f498
Haskman <sahinmehmetutku@gmail.com>
2015-10-25 16:21:55 -0400

    Part 2: Extracts only the measurements on the mean and standard deviation for each measurement.
    The script now extracts means and deviations from each measurement. The
    merged data is also now called initial_data and not final_data

--- run_analysis.R
+++ run_analysis.R
@@ -9,13 +9,13 @@ y_train <- read.table("train//y_train.txt", sep = "\t")
 subject_test <- read.table("test//subject_test.txt", sep = "\t")
 subject_train <- read.table("train//subject_train.txt", sep = "\t")
 
-#Merge test and training sets with with rbind
+#Merge test and training sets with with rbind()
 X_data <- rbind(X_test, X_train)
 y_data <- rbind(y_test,y_train)
 subject_data <- rbind(subject_test, subject_train)
 
-#Merge the merged test and data sets into one final dataset
-final_data <- cbind(X_data, y_data, subject_data)
+#Merge the merged test and data sets into one final dataset with cbind()
+initial_data <- cbind(X_data, y_data, subject_data)
 
 #Clean the variables no longer necessary
 remove(X_test)
@@ -28,7 +28,23 @@ remove(X_data)
 remove(y_data)
 remove(subject_data)
 
-#Extract mean and standard deviation for each measurement. 
+##Extract mean and standard deviation for each measurement using strsplit().
+
+#Create a vector for each wanted value
+
+means <- c()
+std_deviations <- c()
+
+#Loop over column 1 to split measures, convert to numeric and get the means and std.deviations while ignoring NA values
+
+for (entry in initial_data[, 1]){
+  means <- c(means, mean(na.omit(sapply(strsplit(entry, " "), as.numeric))))
+  std_deviations <- c(std_deviations, sd(na.omit(sapply(strsplit(entry, " "), as.numeric))))
+}
+
+#remove unnecessary variable
+remove(entry)
+
 #Uses descriptive activity names to name the activities in the data set
 #Appropriately labels the data set with descriptive variable names. 
 
